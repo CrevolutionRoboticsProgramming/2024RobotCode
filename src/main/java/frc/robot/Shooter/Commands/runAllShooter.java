18c4007298c1282900d7fc2ab4ac162db36a1fe2
Eddie-Edward <111027605+Eddie-Edward@users.noreply.github.com>
2024-02-17 23:35:25 -0500

    Vision Line Up command changes (No Done)

--- src/main/java/frc/robot/Shooter/Commands/runAllShooter.java
+++ src/main/java/frc/robot/Shooter/Commands/runAllShooter.java
@@ -1,32 +1,73 @@
-package frc.robot.Shooter.Commands;
+// package frc.robot.Shooter.Commands;
 
-import java.util.function.DoubleSupplier;
+// import java.util.function.DoubleSupplier;
+// import java.util.function.Supplier;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Shooter.Shooter;
+// import edu.wpi.first.math.controller.ArmFeedforward;
+// import edu.wpi.first.math.controller.PIDController;
+// import edu.wpi.first.math.estimator.PoseEstimator;
+// import edu.wpi.first.math.geometry.Pose2d;
+// import edu.wpi.first.math.trajectory.TrapezoidProfile;
+// import edu.wpi.first.math.util.Units;
+// import edu.wpi.first.wpilibj2.command.Command;
+// import frc.robot.Drivetrain.DrivetrainConfig.DriveConstants;
+// import frc.robot.Shooter.Shooter;
+// import frc.robot.Shooter.ShooterConfig;
+// import frc.robot.Shooter.ShooterIndexer;
+// import frc.robot.Shooter.ShooterPivot;
 
-public class runAllShooter extends Command {
-    private final DoubleSupplier supplier;
-    private final Shooter m_shooter;
+// public class runAllShooter extends Command {
+//   private final ShooterPivot m_Pivot;
+//   private final Shooter m_Shooter;
+//   private final DoubleSupplier supplier;
+//   private final ShooterIndexer m_Index;
+//   private Supplier<Pose2d> poseEst;
 
-    public runAllShooter(Shooter shooter, DoubleSupplier supplier) {
-        this.m_shooter = shooter;
-        this.supplier = supplier;
+//   private double staringAngle, endAngle;
+//   private Long startTs;
+
+//   private TrapezoidProfile profile;
+//   private final ArmFeedforward ffController;
+//   private final PIDController pidController;
+
+//   public runAllShooter(Shooter shooter, ShooterPivot pivot, ShooterIndexer index, Supplier<Pose2d> poseEst, DoubleSupplier supplier) {
+//     this.m_Shooter = shooter;
+//     this.m_Pivot = pivot;
+//     this.supplier = supplier;
+//     this.m_Index = index;
+//     this.poseEst = poseEst;
+
+//     ffController = new ArmFeedforward(ShooterConfig.kS, ShooterConfig.kG, ShooterConfig.kV, ShooterConfig.kA);
+//     pidController = new PIDController(ShooterConfig.kVelP, ShooterConfig.kVelI, ShooterConfig.kVelD);
+
+//     startTs = null;
     
-        addRequirements(m_shooter);
-    }
+//     addRequirements(m_Shooter);
+//   }
 
-    @Override
-    public void initialize() {
-      m_shooter.setShooterPercentOutput(1);
-    }
+//   @Override
+//   public void initialize() {
+//     profile = generateProfile();
+//     if(m_Index.getBeamBreaker()) {
+//       m_Shooter.setShooterPercentOutput(1);
+//     }
+//     else{
+//       m_Shooter.stop();
+//     }
+//   }
 
-    @Override
-    public void execute() {
+//   @Override
+//   public void execute() {
       
-    }
-    @Override
-    public void end(boolean interrupted) {
-      m_shooter.stop();
-    }
-}
+//   }
+//   @Override
+//   public void end(boolean interrupted) {
+//     m_Shooter.stop();
+//   }
+
+//   private TrapezoidProfile generateProfile() {
+//     return new TrapezoidProfile(new TrapezoidProfile.Constraints(DriveConstants.maxAngularVelocity, 720.0), 
+//     new TrapezoidProfile.State(Units.degreesToRadians(endAngle),0),
+//     new TrapezoidProfile.State(Units.degreesToRadians(startingAngle), DriveConstants.maxAngularVelocity));
+//   }
+// }
--- src/main/java/frc/robot/Vision/VisionCommands/VisionLineUp.java
+++ src/main/java/frc/robot/Vision/VisionCommands/VisionLineUp.java
@@ -1,7 +1,11 @@
 package frc.robot.Vision.VisionCommands;
 
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -21,26 +25,32 @@ import frc.robot.Vision.Vision;
 import frc.robot.Vision.VisionConfig;
 import frc.robot.Vision.Vision.PoseEstimator;
 import frc.robot.Vision.VisionConfig.ShooterCamsConfig;
+import org.littletonrobotics.junction.LogTable;
 
 public class VisionLineUp extends Command {
     private final Drivetrain mDrivetrain;
     private final PoseEstimator mPoseEstimator;
-    private final PIDController pidController;
+    // private final PIDController pidController;
+    // private final SimpleMotorFeedforward ffController;
     private TrapezoidProfile profile;
     private Pose2d robotPose;
     private Pose2d goalPose;
-    private State currentState, targetState;
-    private double startingAngle, endAngle;
-    private SwerveModuleState[] modState;
+    private double startingAngle, endAngle, distance;
+    private Long startTs;
+    LogTable table;
+
+    // Robot Radius (diagonal) in meters
+    private final double radius = 0.97 / 2.0; 
     
 
     public VisionLineUp(Drivetrain mDrivetrain, PoseEstimator mPoseEstimator) {
         this.mDrivetrain = mDrivetrain;
         this.mPoseEstimator = mPoseEstimator;
 
-        modState = new SwerveModuleState[4];
+        // modState = new SwerveModuleState[4];
 
-        pidController = new PIDController(DriveConstants.driveKP, DriveConstants.driveKI, DriveConstants.driveKD);
+        // pidController = new PIDController(DriveConstants.driveKP, DriveConstants.driveKI, DriveConstants.driveKD);
+        // ffController = new SimpleMotorFeedforward(DriveConstants.driveKS, DriveConstants.driveKV, DriveConstants.driveKA);
         profile = null;
 
         addRequirements(mDrivetrain, mPoseEstimator);
@@ -48,55 +58,75 @@ public class VisionLineUp extends Command {
 
     @Override
     public void initialize() {
-        profile = generateProfile();
+        var deltaTheta = Units.degreesToRadians(180.0);
+        distance = radius * deltaTheta;
+        profile = generateProfile(distance);
+        startTs = null;
 
-        robotPose = mPoseEstimator.getCurrentPose();
-        var currentAlliance = DriverStation.getAlliance();
-        if(currentAlliance.equals(DriverStation.Alliance.Blue)) {
-            goalPose = new Pose2d(new Translation2d(Units.inchesToMeters(-1.5), Units.inchesToMeters(218.42)), new Rotation2d(0));
-        }
-        else {
-            goalPose = new Pose2d(new Translation2d(Units.inchesToMeters(652.73), Units.inchesToMeters(218.42)), new Rotation2d(Units.degreesToRadians(180)));
-        }
+        // robotPose = mPoseEstimator.getCurrentPose();
+        // var currentAlliance = DriverStation.getAlliance();
+        // if(currentAlliance.equals(DriverStation.Alliance.Blue)) {
+        //     goalPose = new Pose2d(new Translation2d(Units.inchesToMeters(-1.5), Units.inchesToMeters(218.42)), new Rotation2d(0));
+        // }
+        // else {
+        //     goalPose = new Pose2d(new Translation2d(Units.inchesToMeters(652.73), Units.inchesToMeters(218.42)), new Rotation2d(Units.degreesToRadians(180)));
+        // }
     }   
 
     @Override
     public void execute() {
-        final var duration = (double) System.currentTimeMillis() / 1000.0;
+        if (startTs == null) {
+            startTs = System.currentTimeMillis();
+        }
 
-        startingAngle = robotPose.getRotation().getDegrees();
-        double adjacent = Math.abs((robotPose.getX()) - (goalPose.getX()));
-        double opposite = Math.abs((robotPose.getY()) - (goalPose.getY()));
-        endAngle = Math.atan2(adjacent, opposite);
+        // startingAngle = robotPose.getRotation().getDegrees();
+        // double adjacent = Math.abs((robotPose.getX()) - (goalPose.getX()));
+        // double opposite = Math.abs((robotPose.getY()) - (goalPose.getY()));
+        // endAngle = Math.atan2(adjacent, opposite);
 
-        final var targetState = profile.calculate(duration);
+        final var targetState = profile.calculate(getElapsedTime());
 
-        final double pidOutput = pidController.calculate(Units.degreesToRadians(endAngle), targetState.velocity);
-
-        modState[0] = new SwerveModuleState(0.0, new Rotation2d(pidOutput));
-        modState[1] = new SwerveModuleState(0.0, new Rotation2d(pidOutput));
-        modState[2] = new SwerveModuleState(0.0, new Rotation2d(pidOutput));
-        modState[3] = new SwerveModuleState(0.0, new Rotation2d(pidOutput));
-
-        mDrivetrain.setModuleStates(modState);
+        final var currentModuleStates = mDrivetrain.getModuleStates();
+        final var targetModuleStates = new SwerveModuleState[currentModuleStates.length];
+        targetModuleStates[0] = new SwerveModuleState(targetState.velocity, new Rotation2d(Units.degreesToRadians(-45)));
+        targetModuleStates[1] = new SwerveModuleState(targetState.velocity, new Rotation2d(Units.degreesToRadians(225)));
+        targetModuleStates[2] = new SwerveModuleState(targetState.velocity, new Rotation2d(Units.degreesToRadians(45)));
+        targetModuleStates[3] = new SwerveModuleState(targetState.velocity, new Rotation2d(Units.degreesToRadians(135)));
+        mDrivetrain.setModuleStates(targetModuleStates);
 
         // print angle valuess
-        System.out.println("start angle" + startingAngle);
-        System.out.println("end angle" + endAngle);
+        // System.out.println("start angle" + startingAngle);
+        // System.out.println("end angle" + endAngle);
+
+        for (var i = 0; i < targetModuleStates.length; i++) {
+            table.put("target module speed [" + i + "]: " + targetModuleStates[i].speedMetersPerSecond);
+            table.put("Current module speed [" + i + "]: ", currentModuleStates[i].speedMetersPerSecond);
+            
+        }
 
         
     }
+    private double getElapsedTime() {
+        return (startTs == null) ? 0 : ((double) System.currentTimeMillis() - startTs) / 1000.0;
+    }
 
+    @Override
+    public boolean isFinished() {
+        final var time = getElapsedTime();
+        return profile != null && profile.isFinished((double) time);
+    }
 
     @Override
     public void end(boolean interrupted) {
         mDrivetrain.stopSwerve();
     }
 
-    private TrapezoidProfile generateProfile() {
-        return new TrapezoidProfile(new TrapezoidProfile.Constraints(DriveConstants.maxAngularVelocity, 720.0), 
-        new TrapezoidProfile.State(Units.degreesToRadians(endAngle),0),
-        new TrapezoidProfile.State(Units.degreesToRadians(startingAngle), DriveConstants.maxAngularVelocity));
+    private TrapezoidProfile generateProfile(double targetDistance) {
+        return new TrapezoidProfile(
+            new TrapezoidProfile.Constraints(DriveConstants.maxSpeed, 3.0),
+            new TrapezoidProfile.State(targetDistance, 0),
+            new TrapezoidProfile.State(0, 0)
+        );
     }
     
 }
