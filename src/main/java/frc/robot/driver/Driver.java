9ea7ab41cee1d9314aae373ab6344e4ccb6cb71f
Jason Deal <jmdeal@amazon.com>
2024-02-27 14:56:37 -0500

    feat: exp curve for driver controls

--- src/main/java/frc/robot/driver/Driver.java
+++ src/main/java/frc/robot/driver/Driver.java
@@ -3,6 +3,7 @@ package frc.robot.driver;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.crevolib.util.ExpCurve;
 import frc.crevolib.util.Gamepad;
 import frc.robot.drivetrain.Drivetrain;
 import frc.robot.drivetrain.commands.DrivetrainCommands;
@@ -11,14 +12,22 @@ public class Driver extends Gamepad {
     private static class Settings {
         static final int port = 0;
         static final String name = "driver";
+
+        static final double kTranslationExpVal = 2.0;
+        static final double kRotationExpVal = 1.0;
+        static final double kDeadzone = 0.1;
     }
+
+    ExpCurve translationStickCurve;
+    ExpCurve rotationStickCurve;
 
     private static Driver mInstance;
 
     private Driver() {
         super(Settings.name, Settings.port);
 
-        
+        translationStickCurve = new ExpCurve(Settings.kTranslationExpVal, 0, 1, Settings.kDeadzone);
+        rotationStickCurve = new ExpCurve(Settings.kRotationExpVal, 0, 1, Settings.kDeadzone);
     }
 
     public static Driver getInstance() {
@@ -31,9 +40,9 @@ public class Driver extends Gamepad {
     @Override
     public void setupTeleopButtons() {
         leftBumperOnly().onTrue(new InstantCommand(() -> Drivetrain.getInstance().zeroHeading()));
-        
+
         leftTriggerOnly().whileTrue(DrivetrainCommands.driveSlowMode(
-            this::getDriveTranslation, 
+            this::getDriveTranslation,
             this::getDriveRotation));
 
         // right triger run intake
@@ -51,12 +60,13 @@ public class Driver extends Gamepad {
     }
 
     public Translation2d getDriveTranslation() {
-        final var xComponent = -controller.getLeftX();
-        final var yComponent = -controller.getLeftY();
-        return new Translation2d((Math.abs(yComponent) < 0.1) ? 0 : yComponent, (Math.abs(xComponent) < 0.1) ? 0 : xComponent);
+        final var xComponent = translationStickCurve.calculate(controller.getLeftX());
+        final var yComponent = translationStickCurve.calculate(controller.getLeftY());
+        // Components are reversed because field coordinates are opposite of joystick coordinates
+        return new Translation2d(yComponent, xComponent);
     }
 
     public double getDriveRotation() {
-        return -controller.getRightX();
+        return -rotationStickCurve.calculate(controller.getRightX());
     }
 }
