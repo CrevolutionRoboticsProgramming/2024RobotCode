7086a71255d1b326c6f7cabb61f91c48c95de147
Eddie-Edward <111027605+Eddie-Edward@users.noreply.github.com>
2024-02-26 22:24:38 -0500

    runIndexer command added

--- src/main/java/frc/robot/Shooter/Commands/setOutputIndexer.java
+++ src/main/java/frc/robot/Shooter/Commands/setOutputIndexer.java
@@ -1,7 +1,29 @@
 package frc.robot.Shooter.Commands;
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Shooter.ShooterIndexer;
+import frc.robot.intakeroller.IntakeRoller;
+import pabeles.concurrency.IntOperatorTask.Min;
 
 public class setOutputIndexer extends Command {
-    
+    private final ShooterIndexer mIndex;
+    private final DoubleSupplier supplier;
+
+    public setOutputIndexer(DoubleSupplier supplier) {
+        mIndex = ShooterIndexer.getInstance();
+        this.supplier = supplier;
+        addRequirements(mIndex);
+    }
+
+    @Override
+    public void execute() {
+        mIndex.setIntakeSpeed(supplier.getAsDouble());
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        mIndex.stopIndexer();
+    }
 }
