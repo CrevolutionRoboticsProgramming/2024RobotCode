52166df7798f8e15e45c7b52a2baa38289a1ff6d
SatchitK <79611051+SatchitK@users.noreply.github.com>
2024-03-22 16:52:47 -0400

    added finalized autons for Troy

--- src/main/java/frc/robot/auton/AutonMaster.java
+++ src/main/java/frc/robot/auton/AutonMaster.java
@@ -1,17 +1,11 @@
 package frc.robot.auton;
 
-import java.security.AuthProvider;
-
-import com.fasterxml.jackson.databind.util.Named;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
 import com.pathplanner.lib.util.PathPlannerLogging;
 import com.pathplanner.lib.util.ReplanningConfig;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -19,10 +13,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.Robot;
-import frc.robot.RobotContainer;
-import frc.robot.auton.commands.TurnInPlaceCommand;
 import frc.robot.commands.RobotCommands;
 import frc.robot.drivetrain.Drivetrain;
 import frc.robot.intakepivot.commands.IntakePivotCommands;
@@ -68,31 +58,45 @@ public class AutonMaster {
         );
  
         /* Add all auton options here */
-        autonChooser.addOption("center-speaker-3p-right [verified]", AutoBuilder.buildAuto("center-speaker-3-right"));
-        autonChooser.addOption("center-speaker-3p-left", AutoBuilder.buildAuto("center-speaker-3-left"));
-        autonChooser.addOption("center-speaker-2p", AutoBuilder.buildAuto("center-speaker-2"));
-        autonChooser.addOption("right-speaker-2.5p [verified]", AutoBuilder.buildAuto("right-speaker-2.5"));
-        autonChooser.addOption("right-speaker-2p", AutoBuilder.buildAuto("right-speaker-2"));
-        autonChooser.addOption("right-speaker-1-temp", AutoBuilder.buildAuto("right-speaker-1-temp"));
-        autonChooser.addOption("left-speaker-2.5p", AutoBuilder.buildAuto("right-speaker-2.5"));
-        autonChooser.addOption("left-speaker-2p", AutoBuilder.buildAuto("right-speaker-2"));
-        autonChooser.addOption("TEST-center-speaker-3p-right", AutoBuilder.buildAuto("center-speaker-3-right-NEW"));
-        autonChooser.addOption("center-speaker-3.5-neutral", AutoBuilder.buildAuto("center-speaker-3.5-neutral"));
-        // autonChooser.addOption("TEST-center-speaker-4.5p-under-stage", AutoBuilder.buildAuto("CenterSpeaker4.5PieceAuton"));
-        autonChooser.addOption("TEST-center-speaker-4p", AutoBuilder.buildAuto("CenterSpeaker4PieceAuton"));
-        // autonChooser.addOption("TEST-feed-4p-auton", AutoBuilder.buildAuto("Feed4PieceAuton"));
-        autonChooser.addOption("left-speaker-1.5p", AutoBuilder.buildAuto("left-speaker-1.5"));
-        autonChooser.addOption("center-speaker-4-neutral", AutoBuilder.buildAuto("center-speaker-4-neutral"));
-        autonChooser.addOption("Coyle's Stupid Auton", AutoBuilder.buildAuto("coyles-stupid-auton"));
+        // autonChooser.addOption("center-speaker-3p-right [verified]", AutoBuilder.buildAuto("center-speaker-3-right"));
+        // autonChooser.addOption("center-speaker-3p-left", AutoBuilder.buildAuto("center-speaker-3-left"));
+        // autonChooser.addOption("center-speaker-2p", AutoBuilder.buildAuto("center-speaker-2"));
+        // autonChooser.addOption("right-speaker-2.5p [verified]", AutoBuilder.buildAuto("right-speaker-2.5"));
+        // autonChooser.addOption("right-speaker-2p", AutoBuilder.buildAuto("right-speaker-2"));
+        // autonChooser.addOption("right-speaker-1-temp", AutoBuilder.buildAuto("right-speaker-1-temp"));
+        // autonChooser.addOption("left-speaker-2.5p", AutoBuilder.buildAuto("right-speaker-2.5"));
+        // autonChooser.addOption("left-speaker-2p", AutoBuilder.buildAuto("right-speaker-2"));
+        // autonChooser.addOption("TEST-center-speaker-3p-right", AutoBuilder.buildAuto("center-speaker-3-right-NEW"));
+        // autonChooser.addOption("center-speaker-3.5-neutral", AutoBuilder.buildAuto("center-speaker-3.5-neutral"));
+        // // autonChooser.addOption("TEST-center-speaker-4.5p-under-stage", AutoBuilder.buildAuto("CenterSpeaker4.5PieceAuton"));
+        // autonChooser.addOption("TEST-center-speaker-4p", AutoBuilder.buildAuto("CenterSpeaker4PieceAuton"));
+        // // autonChooser.addOption("TEST-feed-4p-auton", AutoBuilder.buildAuto("Feed4PieceAuton"));
+        // autonChooser.addOption("left-speaker-1.5p", AutoBuilder.buildAuto("left-speaker-1.5"));
+        // autonChooser.addOption("center-speaker-4-neutral", AutoBuilder.buildAuto("center-speaker-4-neutral"));
+        // autonChooser.addOption("Coyle's Stupid Auton", AutoBuilder.buildAuto("coyles-stupid-auton"));
         
 
         //SATCHIT AUTOS PRE TROY PLEASE TEST ASAP. DO NOT RUN WITHOUT TESTING !!!!!
         // autonChooser.addOption("OPTIMIZED-PRE-TROY-center-speaker-4p", AutoBuilder.buildAuto("OPTIMIZED-center-speaker-4-piece"));
-        autonChooser.addOption("OPTIMIZED-PRE-TROY-center-speaker-4p", AutoBuilder.buildAuto("OPTIMIZED-center-speaker-non3175-4-piece"));
+        //autonChooser.addOption("OPTIMIZED-PRE-TROY-center-speaker-4p", AutoBuilder.buildAuto("OPTIMIZED-center-speaker-non3175-4-piece"));
         // autonChooser.addOption("OPTIMIZED-PRE-TROY-center-speaker-4.5p", AutoBuilder.buildAuto("OPTIMIZED-center-speaker-4.5-piece"));
         // autonChooser.addOption("OPTIMIZED-PRE-TROY-left-speaker-steal-center-fun", AutoBuilder.buildAuto("OPTIMIZED-left-speaker-steal-center-fun"));
         // autonChooser.addOption("OPTIMIZED-PRE-TROY-center-speaker-middle-4-piece", AutoBuilder.buildAuto("OPTIMIZED-center-speaker-middle-4-piece"));
         // autonChooser.addOption("OPTIMZIED-PRE-TROY-right-speaker-middle-4-piece", AutoBuilder.buildAuto("OPTIMIZED-right-speaker-middle-4-piece"));
+        
+        ////////////////////////////////////////////////////////
+        //////////////////// TROY 1 AUTONS ////////////////////
+        ///////////////////////////////////////////////////////
+        
+        //ALL THE 4 piece or steal autons
+        autonChooser.setDefaultOption("FINALIZED-center-speaker-close-4p", AutoBuilder.buildAuto("FINALIZED-center-speaker-4-piece"));
+        autonChooser.addOption("FINALIZED-center-speaker-steal-4p", AutoBuilder.buildAuto("FINALIZED-center-speaker-steal-4-piece"));
+        autonChooser.addOption("FINALIZED-left-speaker-steal-3p", AutoBuilder.buildAuto("FINALIZED-left-speaker-steal-3-piece"));
+        autonChooser.addOption("FINALIZED-right-speaker-steal-4p", AutoBuilder.buildAuto("FINALIZED-right-speaker-steal-4-piece"));
+        
+        //SAFETY: all the 3 piece autons
+        autonChooser.addOption("FINALIZED-center-speaker-3p-left", AutoBuilder.buildAuto("FINALIZED-center-speaker-3-piece-left"));
+        autonChooser.addOption("FINALIZED-center-speaker-3p-right", AutoBuilder.buildAuto("FINALIZED-center-speaker-3-piece-right"));
         configurePathPlannerLogging();
     }
 
